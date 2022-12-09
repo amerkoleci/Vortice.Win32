a768f59eeae511f04067ae5215e3359a7d9525b2
Amer Koleci <amerkoleci@gmail.com>
2022-12-09 11:06:10 +0100

    Dxc: Expose CLSID_DxcUtils as alias of CLSID_DxcLibrary

--- src/Vortice.Win32.Graphics.Direct3D.Dxc/Apis.cs
+++ src/Vortice.Win32.Graphics.Direct3D.Dxc/Apis.cs
@@ -1,12 +1,12 @@
 // Copyright © Amer Koleci and Contributors.
 // Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
 
-using System.Runtime.CompilerServices;
-
 namespace Win32.Graphics.Direct3D.Dxc;
 
 public static unsafe partial class Apis
 {
+    public static ref readonly Guid CLSID_DxcUtils => ref CLSID_DxcLibrary;
+
     public static HResult DxcCreateInstance(in Guid rclsid, Guid* riid, void** ppv)
     {
         return DxcCreateInstance(
