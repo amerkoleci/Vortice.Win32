df466049b05cd62fb1ae789013fd01f2070eb5ef
Amer Koleci <amerkoleci@gmail.com>
2022-12-23 12:07:12 +0100

    FIX: Apis collision in D3D11 and Fxc.

--- src/Vortice.Win32.Graphics.Direct3D11/Apis.Fxc.cs
+++ src/Vortice.Win32.Graphics.Direct3D11/Apis.Fxc.cs
@@ -1,13 +1,10 @@
 // Copyright © Amer Koleci and Contributors.
 // Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
 
-using Win32.Graphics.Direct3D11;
-
-namespace Win32.Graphics.Direct3D.Fxc;
+namespace Win32.Graphics.Direct3D11;
 
 public static unsafe partial class Apis
 {
-
     [DllImport("D3DCOMPILER_47.dll", ExactSpelling = true)]
     public static extern HResult D3DCreateLinker(ID3D11Linker** ppLinker);
 
